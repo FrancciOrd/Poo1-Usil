728c5166494fec9f970ea7ce43fc251418272e61
Francheska Ordoñez <francheska.ordonez@usil.pe>
2023-09-06 00:20:03 -0500

    prueba

---
+++ GitHubEjercicio.java
@@ -0,0 +1,7 @@
+public class GitHubPractica {
+  public void metodoAlumnoA(){
+    System.out.println("metodo A");
+  }
+  public void metodoColaborativo(){
+    System.out.println( "Hola mundo" );
+}
